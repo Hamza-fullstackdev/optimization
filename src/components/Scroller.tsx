095992e6672d03a4bc95331a531585b1fdfabb6c
hamza-fullstackdev <hamzafullstackdev1@gmail.com>
2026-02-04 15:36:15 +0500

    Showing Posts Using React Virtuoso

--- src/components/Scroller.tsx
+++ src/components/Scroller.tsx
@@ -1,39 +1,19 @@
-import { List } from 'react-window';
+import { Virtuoso } from 'react-virtuoso';
 
 const Scroller = () => {
   return (
-    <List
-      rowComponent={RowComponent}
-      rowCount={5000000}
-      rowHeight={35}
-      rowProps={{
-        data: Array.from({ length: 5000000 }, (_, i) => `POST ${i + 1}`),
-      }}
-      style={{
-        maxHeight: 300,
-      }}
+    <Virtuoso
+      style={{ height: '300px' }}
+      totalCount={5000000}
+      overscan={200}
+      itemContent={(index) => (
+        <div className="flex justify-between items-center p-2 border-gray-200 border-b">
+          Post #{index + 1}
+          <div className="text-slate-500 text-xs">{`${index + 1} of 5000000`}</div>
+        </div>
+      )}
     />
   );
 };
-
-function RowComponent({
-  index,
-  data,
-  style,
-}: {
-  index: number;
-  data: string[];
-  style: React.CSSProperties;
-}) {
-  return (
-    <div
-      className="flex justify-between items-center bg-gray-100 my-2 p-3"
-      style={style}
-    >
-      {data[index]}
-      <div className="text-slate-500 text-xs">{`${index + 1} of ${data.length}`}</div>
-    </div>
-  );
-}
 
 export default Scroller;
